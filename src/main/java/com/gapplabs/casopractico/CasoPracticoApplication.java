30d2cfda88faabb5e78fb8ca63f9f940f945311a
Patrik <psirb@myuax.com>
2024-09-11 10:43:54 +0200

    Initial commit

---
+++ src/main/java/com/gapplabs/casopractico/CasoPracticoApplication.java
@@ -0,0 +1,13 @@
+package com.gapplabs.casopractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPracticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoPracticoApplication.class, args);
+    }
+
+}
